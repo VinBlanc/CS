6bb3333e5b006ae2a8fe835691f9d221c77db1e2
Desktop <k1994pino@gmail.com>
2019-02-02 19:52:25 +0900

    feat: CellShapeCutter

---
+++ CellShapeCutter/CellShapeCutter/Cutter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellShapeCutter
+{
+    public class Cutter
+    {
+        public static List<List<T>> CutToCellShape<T>(
+            List<T> rawData,
+            int cellColNum)
+        {
+            int maxColNum = rawData.Count();
+            var tmp = new List<List<T>> ();
+            
+            for(int i=0; i < maxColNum/cellColNum; i++)
+            {
+                tmp.Add(rawData.GetRange(i*cellColNum, cellColNum));
+            }
+            return tmp;
+        }
+
+        public static List<List<T>> CutToCellShape<T>(
+            List<List<T>> rawData,
+            int cellColNum,
+            int cellRowNum)
+        {
+            int maxRowNum = rawData.Count();
+            int maxColNum = rawData.ElementAt(0).Count();
+
+            var tmp = new List<List<T>>();
+
+            for (int i = 0; i < maxRowNum / cellRowNum; i++)
+            {
+                var line = rawData.GetRange(i * cellRowNum, cellRowNum);
+                for (int j = 0; j < maxColNum / cellColNum; j++)
+                {
+                    var cell = line.SelectMany(
+                        e => e.GetRange(j * cellColNum, cellColNum))
+                        .ToList();
+                    tmp.Add(cell);
+                }
+            }
+            return tmp;
+        }
+
+        private static bool IsCellShapeCorrect(
+            List<List<string>> rawData, 
+            int cellRowNum, 
+            int cellColNum,
+            int maxColNum,
+            int maxRowNum)
+        {
+            if (maxRowNum / cellRowNum != 0) return false;
+            if (maxColNum / cellColNum != 0) return false;
+            return true;
+        }
+
+        private static bool IsSquareShape(
+            List<List<string>> rawData)
+        {
+            int rowNumStd = rawData.ElementAt(0).Count();
+            var a = rawData
+                .Select(line => line.Count())
+                .Where(rowNum => rowNum == rowNumStd);
+
+            return true;
+        }
+    }
+}
---
+++ CellShapeCutter/CellShapeCutter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellShapeCutter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new List<string> { "a1", "a2", "a3", "a4", "a5", "a6" };
+            var b = new List<string> { "b1", "b2", "b3", "b4", "b5", "b6" };
+            //var c = new List<string> { "c1", "c2", "c3", "c4", "c5", "c6" };
+            var alphabet = new List<List<string>>{ a, b};
+            var cellList = Cutter.CutToCellShape(alphabet,2, 2);
+
+            foreach(var list in cellList)
+            {
+                foreach (var e in list)
+                {
+                    Console.Write(e + ",");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
